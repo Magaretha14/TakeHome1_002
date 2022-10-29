ae1eeeaa771c4dbd5c8826972f74c45a5b1c51fe
Magaretha14 <magaretha.wahyunda@gmail.com>
2022-10-29 14:09:46 +0700

    my first commit

---
+++ src/main/java/ws/a/Tugas1WS/Tugas1WsApplication.java
@@ -0,0 +1,13 @@
+package ws.a.Tugas1WS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tugas1WsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tugas1WsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ws/a/Tugas1WS/Tugas1WsApplicationTests.java
@@ -0,0 +1,13 @@
+package ws.a.Tugas1WS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tugas1WsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
