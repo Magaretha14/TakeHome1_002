50a8e5327757c30064ad13f2dab4bb7fc3cc68fe
Magaretha14 <magaretha.wahyunda@gmail.com>
2022-10-30 13:52:47 +0700

    menambahkan code untuk display gambar di view html dengan menggunakan path namun blm berhasil masih retak gambarnya

--- src/main/java/ws/a/Tugas1WS/TheController.java
+++ src/main/java/ws/a/Tugas1WS/TheController.java
@@ -5,6 +5,10 @@
  */
 package ws.a.Tugas1WS;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.sql.Blob;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,16 +24,23 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 public class TheController {
     
+    public static String UPLOAD_DIRECTORY = System.getProperty("user.dir");
+    
     @RequestMapping("/hiling")
     public String tampil(
             @RequestParam(value = "nama") String isinama,
             @RequestParam(value = "lokasi") String isilokasi,
-            @RequestParam(value = "gambar") MultipartFile isigambar,
+            @RequestParam(value = "gambar") MultipartFile file,
             Model kurir
-    ){
+    ) throws IOException{
+        StringBuilder fileNames = new StringBuilder();
+        Path fileNameAndPath = Paths.get(UPLOAD_DIRECTORY, file.getOriginalFilename());
+        fileNames.append(file.getOriginalFilename());
+        Files.write(fileNameAndPath, file.getBytes());
+        kurir.addAttribute("msg", file);
+        
         kurir.addAttribute("paketnama", isinama);
         kurir.addAttribute("paketlokasi", isilokasi);
-        kurir.addAttribute("paketimg", isigambar);
         
         return "view";
     }
