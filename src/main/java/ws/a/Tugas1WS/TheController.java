d34be9581bbf11974aac216d55678716ab26a5f6
Magaretha14 <magaretha.wahyunda@gmail.com>
2022-10-31 13:18:05 +0700

    menambahkan css pada index dan view html

--- src/main/java/ws/a/Tugas1WS/TheController.java
+++ src/main/java/ws/a/Tugas1WS/TheController.java
@@ -9,7 +9,6 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.sql.Blob;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
